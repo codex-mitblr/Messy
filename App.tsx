c80f7cb88d5ef56f64fbf5eeb78ce83505136da1
Chandan <chandancvp08@gmail.com>
2022-10-23 12:14:49 +0530

    added basic stack navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Messy</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title:"Home Page", headerTitleAlign:"center"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { Button } from 'react-native-paper'
 
 function HomeScreen() {
   return (
-    <View>
-        <Text>Home screen</Text>
+    <View style={styles.container}>
+       <Button onPress={()=>{}} mode="contained" style={styles.MessButtons}>Veg Mess</Button>
+       <Button onPress={()=>{}} mode="contained" style={styles.MessButtons}>Non-Veg Mess</Button>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container:{
+padding:20
+  },
+  MessButtons:{
+    margin:10
+  }
+})
 export default HomeScreen
